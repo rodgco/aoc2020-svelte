496dded091853fffc9314b530b06402566b39ab5
Rodrigo Couto <r@rodg.co>
2020-12-01 09:27:04 -0300

    feat: :sparkles: Day 1 is solved

---
+++ src/puzzles/Day1.svelte
@@ -0,0 +1,29 @@
+<script>
+  import data from "../input/day1.js";
+
+  let threeSomeFirst, threeSomeSecond, threeSomeThird;
+  [threeSomeFirst, threeSomeSecond, threeSomeThird] = data.findThreeThatSumTo(
+    2020
+  );
+
+  let twoSomeFirst, twoSomeSecond;
+  [twoSomeFirst, twoSomeSecond] = data.findTwoThatSumTo(2020);
+</script>
+
+<h2>Day1</h2>
+<h3>First Puzzle</h3>
+<p>
+  First number is
+  {twoSomeFirst}, second number is
+  {twoSomeSecond}. They times
+  {twoSomeFirst * twoSomeSecond}
+</p>
+
+<h3>Second Puzzle</h3>
+<p>
+  First number is
+  {threeSomeFirst}, second number is
+  {threeSomeSecond}, third is
+  {threeSomeThird}. They times
+  {threeSomeFirst * threeSomeSecond * threeSomeThird}
+</p>
